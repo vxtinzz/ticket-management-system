2ac611484b802776555772f7559b8dca6e775c51
vxtinzz <vitin.ti679@gmail.com>
2026-09-16 08:51:05 -0300

    feat: update routes

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import ticketRoutes from "./routes/ticketRoutes"
+import responsibleRoutes from "./routes/reponsibleRoutes"
+
 
 const app = express();
 app.use(express.json({limit : "10kb"}))
@@ -8,5 +11,13 @@ app.get("/health", (_req, res) => {
     status: "ok",
   });
 });
+
+app.use(express.json({limit : "10kb"}))
+app.use("/tickets", ticketRoutes)
+app.use("/responsibles", responsibleRoutes)
+
+app.use((_req, res) => {
+    res.status(404)
+})
     
 export default app;
